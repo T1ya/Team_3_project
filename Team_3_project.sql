8fde263c7fc688a6095793a4b94f39bc1fd07d23
T1ya <59089232+T1ya@users.noreply.github.com>
2025-06-02 18:46:53 +0200

    Update Team_3_project.sql
    updated tables structure

--- Team_3_project.sql
+++ Team_3_project.sql
@@ -1,64 +1,93 @@
+-- Создание базы данных
 CREATE DATABASE group_project;
 
---add student
+-- Таблица студентов
 CREATE TABLE student (
-  id serial PRIMARY KEY,
-  name varchar(50),
- 	last_name varchar(50)
- );
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    last_name VARCHAR(50),
+    UNIQUE(name, last_name)
+);
 
---fill student
-INSERT INTO student (name, last_name) VALUES 
-  ('John', 'Doe'),
-  ('Jane', 'Lane'),
-  ('David', 'Martinez'),
-  ('Micaella', 'Dowson'),
-  ('Hans', 'Kohl'),
-  ('Elke', 'Shickert'),
-  ('Maria', 'Karrey')
+-- Таблица преподавателей
+CREATE TABLE teacher (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(80) UNIQUE
+);
 
---add discipline
+-- Таблица курсов
+CREATE TABLE course (
+    id SERIAL PRIMARY KEY,
+    course_name VARCHAR(80)
+);
+
+-- Таблица дисциплин
 CREATE TABLE discipline (
-  id serial PRIMARY KEY,
-  title varchar(80)
-  );
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(80),
+    course_id INT REFERENCES course (id) ON DELETE CASCADE
+);
 
---fill discipline list
-INSERT INTO discipline (title) VALUES 
-('Mathematic methods'),
-('Linear algebra'),
-('English'),
-('German'),
-('Philosophy'),
-('Python'),
-('C/C++')
-
---binding table discipline_student
+-- Связующая таблица студенты-дисциплины
 CREATE TABLE discipline_student (
-  student_id int REFERENCES student(id) ON DELETE CASCADE,
-  discipline_id int REFERENCES discipline(id) ON DELETE CASCADE,
-  grade int,
-  PRIMARY KEY (student_id, discipline_id)
-  );
-  
---fill binding table discipline_student
-INSERT INTO discipline_student(student_id, discipline_id, grade) 
-VALUES
-(1,1,5),
-(1,2,5),
-(1,3,4),
-(1,4,4),
-(1,5,3),
-(1,6,4),
---(1,6,5),
-(1,7,5);
+    student_id INT REFERENCES student (id) ON DELETE CASCADE,
+    discipline_id INT REFERENCES discipline (id) ON DELETE CASCADE,
+    grade INT CHECK(grade BETWEEN 1 AND 5),
+    PRIMARY KEY (student_id, discipline_id)
+);
 
-DROP TABLE discipline_student;
+-- Связующая таблица преподаватели-дисциплины
+CREATE TABLE teacher_discipline (
+    teacher_id INT REFERENCES teacher (id) ON DELETE CASCADE,
+    discipline_id INT REFERENCES discipline (id) ON DELETE CASCADE,
+    PRIMARY KEY (teacher_id, discipline_id)
+);
 
-SELECT student.name AS student, discipline.title AS discipline, grade 
-FROM discipline_student
-JOIN student ON student.id = discipline_student.student_id
-JOIN discipline ON discipline.id = discipline_student.discipline_id;
+-- Заполнение таблицы курсов
+INSERT INTO course (course_name) VALUES
+    ('Mathematics'),
+    ('Programming'),
+    ('Communications');
 
+-- Заполнение таблицы дисциплин
+INSERT INTO discipline (title, course_id) VALUES
+    ('Mathematical Methods', 1),
+    ('Linear Algebra', 1),
+    ('English', 3),
+    ('German', 3),
+    ('Philosophy', 3),
+    ('Python', 2),
+    ('C/C++', 2);
 
+-- Заполнение таблицы студентов
+INSERT INTO student (name, last_name) VALUES
+    ('John', 'Doe'),
+    ('Jane', 'Lane'),
+    ('David', 'Martinez'),
+    ('Micaella', 'Dowson'),
+    ('Hans', 'Kohl'),
+    ('Elke', 'Shickert'),
+    ('Maria', 'Karrey');
 
+-- Заполнение таблицы преподавателей
+INSERT INTO teacher (name) VALUES
+    ('Einstein'),
+    ('Laplace'),
+    ('Shakespeare'),
+    ('Goethe'),
+    ('Schopenhauer'),
+    ('Stroustrup'),
+    ('Van Rossum')
+    ('D Alembert');
+
+-- Заполнение связующей таблицы дисциплины-студенты
+INSERT INTO discipline_student (student_id, discipline_id, grade) VALUES
+    (1, 1, 5), (1, 2, 5), (1, 3, 4), (1, 4, 4), (1, 5, 3), (1, 6, 4), (1, 7, 5),
+    (2, 1, 4), (2, 2, 3), (2, 6, 5), (2, 7, 5),
+    (3, 3, 5), (3, 4, 5), (3, 5, 4),
+    (4, 6, 3), (4, 7, 4),
+    (5, 1, 5), (5, 2, 4), (5, 3, 4);
+
+-- Заполнение связующей таблицы преподаватели-дисциплины
+INSERT INTO teacher_discipline (teacher_id, discipline_id) VALUES
+    (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7);
